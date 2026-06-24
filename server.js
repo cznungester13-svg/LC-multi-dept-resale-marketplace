b8e136fcd71058a8294e6d861b2eeeed243962b3
cznungester13-svg <cznungester13@gmail.com>
2026-06-23 23:48:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-import express from "express";
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "Marketplace API Running" });
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+your-project/server.js
